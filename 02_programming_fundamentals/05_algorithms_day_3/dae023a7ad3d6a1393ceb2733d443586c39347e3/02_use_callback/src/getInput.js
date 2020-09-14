f843fb683ee02fc6c183d2f28b898bd070d1a6a1
Remilde <remilde@outlook.fr>
2020-09-14 16:31:59 +0200

    Clean tree

---
+++ 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/02_use_callback/src/getInput.js
@@ -0,0 +1,4 @@
+// A function that takes two parameters, the second one being a callback function
+const getInput = (inputGet, callback) => callback(inputGet.key); // code the function here
+// Leave the line below for tests to work
+module.exports = getInput;
