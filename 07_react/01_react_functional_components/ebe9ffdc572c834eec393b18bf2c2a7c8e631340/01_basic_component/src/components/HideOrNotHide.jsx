2ef334f0ffb4fe8cadceaea1d21086dc6c3e8d2d
Remilde <remilde@outlook.fr>
2020-11-03 11:22:44 +0100

    fin exercice 1

--- 07_react/01_react_functional_components/ebe9ffdc572c834eec393b18bf2c2a7c8e631340/01_basic_component/src/components/HideOrNotHide.jsx
+++ 07_react/01_react_functional_components/ebe9ffdc572c834eec393b18bf2c2a7c8e631340/01_basic_component/src/components/HideOrNotHide.jsx
@@ -2,4 +2,17 @@ import React from "react";
 
 export const HideOrNotHide = () => {
   // Code here
+  const [isHide, setHide] = React.useState(true);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setHide(!isHide);
+        }}
+      >
+        {isHide ? "Hide content" : "Reveal content"}
+      </button>
+      {isHide ? <p>Some text</p> : null}
+    </div>
+  );
 };
